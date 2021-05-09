e86ff4bc53ae0b47a2d393b9a1ff452b5f970270
Pupendo <michal.pupak@gmail.com>
2021-05-09 18:03:57 +0200

    Migration

---
+++ WebAPI/Migrations/20210502120837_AdultJob.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApi.Migrations
+{
+    public partial class AdultJob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JobId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    HairColor = table.Column<string>(type: "TEXT", nullable: false),
+                    EyeColor = table.Column<string>(type: "TEXT", nullable: false),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Weight = table.Column<float>(type: "REAL", nullable: false),
+                    Height = table.Column<int>(type: "INTEGER", nullable: false),
+                    Sex = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adults", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JobTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    Salary = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.JobId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adults");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
---
+++ WebAPI/Migrations/20210502135944_AdultJob2.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApi.Migrations
+{
+    public partial class AdultJob2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                table: "Adults");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTitle",
+                table: "Jobs",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobTitleJobId",
+                table: "Adults",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adults_JobTitleJobId",
+                table: "Adults",
+                column: "JobTitleJobId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Adults_Jobs_JobTitleJobId",
+                table: "Adults",
+                column: "JobTitleJobId",
+                principalTable: "Jobs",
+                principalColumn: "JobId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Adults_Jobs_JobTitleJobId",
+                table: "Adults");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Adults_JobTitleJobId",
+                table: "Adults");
+
+            migrationBuilder.DropColumn(
+                name: "JobTitleJobId",
+                table: "Adults");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTitle",
+                table: "Jobs",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobId",
+                table: "Adults",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
